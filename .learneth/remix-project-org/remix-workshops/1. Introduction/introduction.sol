60a43eb147fd63f4ebcda3de4e20c552c6ac8918
kpalaypu <kautilya.palaypu@gmail.com>
2025-09-27 01:19:36 -0700

    commit on ERC20_Tutorial

--- .learneth/remix-project-org/remix-workshops/1. Introduction/introduction.sol
+++ .learneth/remix-project-org/remix-workshops/1. Introduction/introduction.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.3;
 
 contract Quiz {
-    uint8 public question1 = 0;
-    uint8 public question2 = 0;
-    uint8 public question3 = 0;
+    uint8 public question1 = 2;
+    uint8 public question2 = 3;
+    uint8 public question3 = 2;
 }
